693f7c62fb5086542507eca41d57fffcb3209172
Hridu06 <sabbirhossainhridoy06@gmail.com>
2023-06-16 20:45:17 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,77 @@
+let bgcolor = document.getElementById('iits-bg');
+bgcolor.classList.remove('bg-danger');
+bgcolor.style.background = 'green';
+
+let img = document.getElementById('iits-img');
+img.src = './image/Hridoy.jpg';
+
+let cr = document.getElementById('iits-cr-msg');
+cr.textContent = 'This is copyright message';
+
+let button = document.getElementById('iits-btn');
+button.style. background = 'green';
+
+let text =  document.getElementById('iits-name');
+text.textContent = 'Sabbir Hossain Hridoy';
+text.classList.remove('text-danger');
+text.style.color = 'green';
+
+let text1=  document.getElementById('iits-prof');
+text1.textContent = 'Web Developer';
+text1.classList.remove('text-danger');
+text1.style.color = 'green';
+
+let text2=  document.getElementById('iits-edu');
+text2.textContent = 'Bachelor Degree';
+text2.classList.remove('text-danger');
+text2.style.color = 'green';
+
+let text3=  document.getElementById('iits-study');
+text3.textContent = 'Computer Science';
+text3.classList.remove('text-danger');
+text3.style.color = 'green';
+
+let text4=  document.getElementById('iits-school');
+text4.textContent = 'IUBAT- International University of Businees Agriculture and Technology';
+text4.classList.remove('text-danger');
+text4.style.color = 'green';
+
+let text5=  document.getElementById('iits-passion');
+text5.textContent = 'Javascript';
+text5.classList.remove('text-danger');
+text5.style.color = 'green';
+
+
+let text6=  document.getElementById('iits-numYears');
+text6.textContent = '2 years';
+text6.classList.remove('text-danger');
+text6.style.color = 'green';
+
+let text7=  document.getElementById('iits-hobby');
+text7.textContent = 'coding';
+text7.classList.remove('text-danger');
+text7.style.color = 'green';
+
+let text8=  document.getElementById('iits-crc');
+text8.textContent = 'Optimistic';
+text8.classList.remove('text-danger');
+text8.style.color = 'green';
+
+let text9=  document.getElementById('iits-ability');
+text9.textContent = 'be self-confident';
+text9.classList.remove('text-danger');
+text9.style.color = 'green';
+
+let text10=  document.getElementById('iits-goal');
+text10.textContent = 'be a computer science engineer';
+text10.classList.remove('text-danger');
+text10.style.color = 'green';
+
+let fb =  document.getElementById('iits-fb-link');
+fb.href = "https://www.facebook.com/profile.php?id=100012826028757";
+
+let mail =  document.getElementById('iits-email');
+mail.href = "sabbirhossainhridoy06@gmail.com";
+
+let github=  document.getElementById('iits-github-link');
+github.href = "https://github.com/Hridu06";
